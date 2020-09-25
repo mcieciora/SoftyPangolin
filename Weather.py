8dd20fa27c48ae2f8a6fdb76a6bb1e5de51ff9ae
mcieciora <michalcieciora96@gmail.com>
2020-09-25 14:43:48 +0200

    25092020-1443

---
+++ Weather.py
@@ -0,0 +1,44 @@
+from Parser import Parser
+from datetime import datetime
+
+
+class Weather:
+    def __init__(self, request):
+        self.request = request
+
+    @property
+    def request(self):
+        return self.__request
+
+    @request.setter
+    def request(self, data):
+        if type(data) is dict:
+            self.__request = data
+
+    def parse_weather_data(self):
+        self.request['weather'] = self.request['weather'][0]['description'].capitalize()
+        self.request['sunrise'] = datetime.fromtimestamp(self.request['sunrise']).strftime('%H:%M')
+        self.request['sunset'] = datetime.fromtimestamp(self.request['sunset']).strftime('%H:%M')
+        self.request['date'] = datetime.now().strftime('%d-%m-%Y %H:%M')
+
+        units = {
+            'temp': '°C',
+            'feels_like': '°C',
+            'pressure': 'hpa',
+            'wind_speed': 'm/s',
+            'humidity': '%%',
+            'clouds': '%%',
+            'weather': '',
+            'sunrise': '',
+            'sunset': '',
+            'date': ''
+        }
+
+        for key in list(self.request):
+            if key in units.keys():
+                if type(self.request[key]) is float:
+                    self.request[key] = round(self.request[key], 1)
+                self.request[key] = str(self.request[key]) + units[key]
+            else:
+                self.request.pop(key)
+        Parser.save_data(self.request, 'data/weather.ini')
